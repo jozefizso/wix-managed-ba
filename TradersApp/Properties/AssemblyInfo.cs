a181d73c4e21ead4f9a74ac28359180d68a3488a
Jozef Izso <jozef.izso@gmail.com>
2017-02-03 20:57:24 +0100

    Sample WPF application

---
+++ TradersApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General information about assembly
+[assembly: AssemblyTitle("TradersApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Northwind Traders")]
+[assembly: AssemblyProduct("TradersApp")]
+[assembly: AssemblyCopyright("Copyright © 2017 Jozef Izso")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
