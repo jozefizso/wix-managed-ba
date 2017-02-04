420a4fa0366358f94a5be79d8d095779ac385545
Jozef Izso <jozef.izso@gmail.com>
2017-02-04 11:54:24 +0100

    Use new Main() entry point

--- TradersApp/App.xaml.cs
+++ TradersApp/App.xaml.cs
@@ -13,5 +13,9 @@ namespace TradersApp
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.InitializeComponent();
+        }
     }
 }
