bf185a13e13e9e9906b40e83815805a055f2fa4d
Jozef Izso <jozef.izso@gmail.com>
2017-03-01 17:08:19 +0100

    Add project for managed bootstrapper application UI

---
+++ TradersBootstrapperApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using TradersBootstrapper.Installer;
+
+// General information about assembly
+[assembly: AssemblyTitle("TradersBootstrapperApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Northwind Traders")]
+[assembly: AssemblyProduct("TradersApp")]
+[assembly: AssemblyCopyright("Copyright © 2017 Jozef Izso")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: BootstrapperApplication(typeof(TradersBootstrapperApp))]
---
+++ TradersBootstrapperApp/TradersBootstrapperApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+
+namespace TradersBootstrapper.Installer
+{
+    public class TradersBootstrapperApp : BootstrapperApplication
+    {
+        protected override void Run()
+        {
+            
+        }
+    }
+}
