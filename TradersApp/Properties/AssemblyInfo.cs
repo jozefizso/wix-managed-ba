c18e491e25f7b0c2abfca8d29aa02b4983ce8b3d
Jozef Izso <jozef.izso@gmail.com>
2020-09-13 13:07:39 +0200

    Traders App release v1.2

--- TradersApp/Properties/AssemblyInfo.cs
+++ TradersApp/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
